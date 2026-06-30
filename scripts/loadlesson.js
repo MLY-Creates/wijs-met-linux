371a43e4259bcb1be3e3603f0341bfabe0a8c5c9
mly <1066300@hr.nl>
2026-06-30 11:45:12 +0200

    Changes to loadlesson

--- scripts/loadlesson.js
+++ scripts/loadlesson.js
@@ -6,31 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 async function loadLesson(filePath, event) {
     const start = performance.now();
-    if (event) {
-        event.preventDefault();
-    }
+    event.preventDefault();
     const container = document.getElementById('lesson-container');
+    container.innerHTML = '<p>Aan het laden...</p>';
 
     try {
-        container.innerHTML = '<p>Aan het laden...</p>';
         const response = await fetch(filePath);
-    
         if (!response.ok) {
             throw new Error(`Lesson not found: ${response.statusText}`);
-        } else {
-            const markdownText = await response.text();
-            container.innerHTML = marked.parse(markdownText);
-            const end = performance.now();
-            console.log(`Time: ${end - start} ms`);
-            if (document.getElementById('bios-tool')) {
-                biosScreen();
-            }
         }
+        container.innerHTML = marked.parse(await response.text());
+        if (document.getElementById('bios-tool')) {
+            biosScreen();
+        }
+        const end = performance.now();
+        console.log(`Time: ${end - start} ms`);
     } catch (error) {
         console.error("Error loading lesson:", error);
-        container.innerHTML =
-            `
+        container.innerHTML = `
             <h2>Er ging iets fout</h2>
             <p>Tux de pinguïn heeft lang en ver gezocht, maar niks gevonden. Weet u zeker dat u met het internet verbonden bent?</p>
-            `;
-    }}
+        `;
+    }
+}
